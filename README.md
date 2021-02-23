adb5c3aeb3f848b4bd8f07020fb1d7c1568d5d26
Zachary Hansen Terry <zmht25@gmail.com>
2021-02-23 14:19:47 -0800

    I added a window and game class

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # My platformer game
-This is a little platformer game I am making with Java. I hope you like it!  
+This is a little platformer game I am making with Java. I hope you like it!
 
-To run the game go to the out directory and type: `java -jar platformer.jar`
+to run the game go to `out/artifacts/platformer_jar/` and type `java -jar platformer.jar`
--- src/com/zachary/platformer/Game.java
+++ src/com/zachary/platformer/Game.java
@@ -1,6 +1,113 @@
 package com.zachary.platformer;
 
-public class Game {
+import java.awt.Canvas;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.image.BufferStrategy;
+import java.util.Random;
+
+
+public class Game extends Canvas implements Runnable{
+
+
+    private static final long serialVersionUID = 1550691097823471818L;
+
+    public static final int WIDTH = 640;
+    public static final int HEIGHT = WIDTH / 12 * 9;
+    private Thread thread;
+    private boolean running = false;
+
+    public enum STATE{
+        Menu,
+        Help,
+        Game;
+    }
+
+    public STATE gameState = STATE.Menu;
+
+    public Game() {
+
+        new Window(WIDTH, HEIGHT,"Lets Build a Game!", this);
+
+
+    }
+
+    public synchronized void start() {
+        thread = new Thread(this);
+        thread.start();
+        running = true;
+    }
+
+    public synchronized void stop() {
+        try {
+            thread.join();
+            running = false;
+        }catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void run() {
+        this.requestFocus();
+        long lastTime = System.nanoTime();
+        double amountOfTicks = 60.0;
+        double ns = 1000000000 / amountOfTicks;
+        double delta = 0;
+        long timer = System.currentTimeMillis();
+        int frames = 0;
+        while(running) {
+            long now = System.nanoTime();
+            delta += (now - lastTime) / ns;
+            lastTime = now;
+            while(delta >= 1) {
+                tick();
+                delta--;
+            }
+            if (running)
+                render();
+            frames++;
+
+            if(System.currentTimeMillis() - timer > 1000) {
+                timer += 1000;
+                System.out.println("FPS: " + frames);
+                frames = 0;
+            }
+        }
+        stop();
+
+    }
+
+    private void tick() {
+
+    }
+
+    private void render() {
+        BufferStrategy bs = this.getBufferStrategy();
+        if (bs == null) {
+            this.createBufferStrategy(3);
+            return;
+        }
+
+
+        Graphics g = bs.getDrawGraphics();
+
+        g.setColor(Color.red);
+        g.fillRect(0, 0, WIDTH, HEIGHT);
+
+
+        g.dispose();
+        bs.show();
+    }
+
+    public static int clamp(float var, float min, float max) {
+        if (var >= max) return (int) (var = max);
+        else if (var <= min) return (int) (var = min);
+        else return (int) var;
+    }
+
+    public static void main(String[] args) {
+        new Game();
+    }
 
 
 }
